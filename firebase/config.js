053bee92265fcbab82122b5f0e8ef8f352a05f42
Anshu <anshujamare10@gmail.com>
2025-06-22 20:20:42 +0530

    Deploy-ready version of E-Bus Management System

--- firebase/config.js
+++ firebase/config.js
@@ -1,21 +1,22 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase/config.js
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase compat version setup (suitable for script-based HTML apps)
 const firebaseConfig = {
   apiKey: "AIzaSyDTNT5g94Qyj1-vDjhapA_uPlbRH53d0kA",
   authDomain: "e-bus-management-system-45082.firebaseapp.com",
   projectId: "e-bus-management-system-45082",
-  storageBucket: "e-bus-management-system-45082.firebasestorage.app",
+  storageBucket: "e-bus-management-system-45082.appspot.com",
   messagingSenderId: "512855889809",
   appId: "1:512855889809:web:387f41b9b144bf84e2ac30",
   measurementId: "G-EYB6VSSBJ6"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Initialize Firebase using compat version
+firebase.initializeApp(firebaseConfig);
+
+// Setup global access for Firestore and Auth
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+window.db = db;
+window.auth = auth;
